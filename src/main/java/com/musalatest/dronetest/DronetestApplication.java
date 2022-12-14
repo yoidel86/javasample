bb1ebb3afe7400f7b126f3f693a8b2bf6d42126c
Yoidel Martinez <yoidel86@gmail.com>
2022-12-13 20:02:54 -0500

    Empty Spring Boot project first commit

---
+++ src/main/java/com/musalatest/dronetest/DronetestApplication.java
@@ -0,0 +1,13 @@
+package com.musalatest.dronetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DronetestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musalatest/dronetest/DronetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musalatest.dronetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronetestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
