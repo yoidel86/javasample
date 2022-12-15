f9e34a1be1d6cac5f67d9f97a918dc1f46271915
Yoidel Martinez <yoidel86@gmail.com>
2022-12-14 23:32:31 -0500

    adding data insertion

---
+++ src/main/resources/db/migrations/V1__load_data.sql
@@ -0,0 +1,21 @@
+insert into
+    DRONE(serial_number, model, weight_limit, battery_capacity, state)
+    values('13579', 'Heavyweight', 500, 98, 'RETURNING');
+insert into
+    DRONE(serial_number, model, weight_limit, battery_capacity, state)
+    values('2222', 'Middleweight', 250, 50, 'DELIVERED');
+insert into
+    DRONE(serial_number, model, weight_limit, battery_capacity, state)
+    values('1111', 'Lightweight', 100, 40, 'LOADING');
+insert into
+    DRONE(serial_number, model, weight_limit, battery_capacity, state)
+    values('3333', 'Cruiserweight', 400, 20, 'IDLE');
+
+
+insert into
+    MEDICATION(name, code, weight, image)
+    values('PFIZER1', 'PFZ-625', 28, 'http://localhost:8080/public/PFIZER1.png');
+
+insert into
+    MEDICATION(name, code, weight, image)
+    values('PFIZER2', 'PFZ-689', 32, 'http://localhost:8080/public/PFIZER2.png');
